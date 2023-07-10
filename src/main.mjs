0d3c6a6bea56781b04cbb2b770374541deb01821
Auke van Slooten <auke@muze.nl>
2023-07-10 14:06:43 +0200

    added array-where-select added meta.references to allow searching for objects/arrays linking to an object (parent relation) improved link replacer added deepFreeze to make all data immutable

--- src/main.mjs
+++ src/main.mjs
@@ -3,9 +3,9 @@ import fs from 'fs'
 import pointer from 'json-pointer'
 import JSONTag from '@muze-nl/jsontag'
 import {JSONPath} from 'jsonpath-plus'
-import jsonExt from '@discoveryjs/json-ext'
-//import TripleStore from './triplestore.mjs'
+import TripleStore from './triplestore.mjs'
 import {VM} from 'vm2'
+import _ from 'array-where-select'
 
 const server    = express()
 const port      = process.env.NODE_PORT || 3000;
@@ -14,17 +14,84 @@ const datafile  = process.env.DATAFILE || 'data.jsontag'
 
 server.use(express.static(process.cwd()+'/www'))
 
+function deepFreeze(obj) {
+  Object.freeze(obj)
+  Object.keys(obj).forEach(prop => {
+    if (typeof obj[prop] === 'object' && !Object.isFrozen(obj[prop])) {
+      deepFreeze(obj[prop])
+    }
+  })
+  return obj
+}
+
+function createReference(meta, obj, prop, value) {
+	if (!meta.references) {
+		meta.references = new Map()
+	}
+	if (!meta.references.has(value)) {
+		meta.references.set(value, {})
+	}
+	let refs = meta.references.get(value)
+	if (!refs[prop]) {
+		refs[prop] = []
+	}
+	refs[prop].push(obj)
+}
+
+let seenRefs = new WeakMap()
+function indexReferences(obj, meta) {
+	if (seenRefs.has(obj)) {
+		//console.log('seen', obj)
+		return
+	}
+	seenRefs.set(obj, true)
+	Object.entries(obj).forEach(([prop,val]) => {
+		if (Array.isArray(val)) {
+			val.forEach(val => {
+				if (val && typeof val == 'object') {
+					createReference(meta, obj, prop, val)
+					indexReferences(val, meta)
+				}
+			})
+		} else if (JSONTag.getType(val) == 'object') {
+			createReference(meta, obj, prop, val)
+			indexReferences(val, meta)
+		} else {
+//			console.log('prop', prop, 'skipped '+val)
+		}
+	})
+}
+
 async function main() {
 
 	const originalJSON = JSON
 	JSON = JSONTag // monkeypatching
 
 	console.log('loading data...')
-	let file = fs.readFileSync(datafile);
-	let dataspace = JSONTag.parse(file.toString());
-	console.log('indexing data...')
-//	let tripleStore = new TripleStore(dataspace)
+	let file = fs.readFileSync(datafile)
+	let dataspace;
+	let meta = {
+		references: new WeakMap()
+	}
+	try {
+		dataspace = JSONTag.parse(file.toString(), null, meta)
+		deepFreeze(dataspace)
+	} catch(e) {
+		console.error(e)
+		process.exit()
+	}
 
+	console.log('indexing data...')
+	indexReferences(dataspace, meta)
+
+	console.log('creating triplestore...')
+	let tripleStore;
+	try {
+		tripleStore = new TripleStore(dataspace)
+	} catch(e) {
+		console.error(e)
+		process.exit()
+	}
 	let used = Math.round(process.memoryUsage().heapUsed / 1024 / 1024);
 	console.log(`data loaded (${used} MB)`);
 
@@ -94,58 +161,6 @@ async function main() {
 	 */
 	server.post('/query/*',  (req, res) => {
 		let start = Date.now()
-		function parseParams(paramsString) {
-			let result = {}
-			let lineRe = /^(.*)$/m
-			let parseRe = /^\s*([a-z].*?)?\s*(\{)?\s*$/i
-			let line = lineRe.exec(paramsString).pop()
-			let full, prop, recurse
-			do {
-				paramsString = paramsString.substring(line.length+1)
-				let parsed = parseRe.exec(line)
-				if (parsed) {
-					[full,prop,recurse] = parsed
-				}
-				if (recurse) {
-					[ result[prop], paramsString ] = parseParams(paramsString)
-				} else if (prop) {
-					result[prop] = ''
-				} else if (/\}/.exec(line)) {
-					return [ result, paramsString ]
-				}
-				line = lineRe.exec(paramsString).pop()
-			} while(line)
-			return [ result, '' ]
-		}
-
-		function filterProperties(paramsOb) {
-			return function(object) {
-				let result = {}
-				let obType = JSONTag.getType(object)
-				JSONTag.setType(result, obType)
-				JSONTag.setAttributes(result, JSONTag.getAttributes(object))
-				Object.entries(paramsOb).forEach(([key,value]) =>{
-					let alias, queryResult
-					[alias,key] = key.split(':',2).map(p => p.trim())
-					if (!key) {
-						key = alias
-					}
-					if (key[0]==='$') {
-						queryResult = JSONPath({path:key, json:object, flatten: true})
-					} else {
-						queryResult = object[key]
-					}
-					if (!value) {
-						// @TODO: just set the entire value for now, should run linkReplacer here
-						// this is better solved after forcing all objects to have an id attribute with a unique id
-						result[alias] = queryResult
-					} else {
-						result[alias] = filterProperties(value)(queryResult)
-					}
-				})
-				return result;
-			}
-		}
 
 		let accept = req.accepts(['application/jsontag','application/json','text/html','text/javascript','image/*'])
 		if (!accept) {
@@ -188,13 +203,16 @@ async function main() {
 //				timeout: 1000,
 				allowAsync: false,
 				sandbox: {
-//					query: function(params) {
-//						return tripleStore.query(params)
-//					}
+					root: dataspace,
+					data: result,
+					meta: meta,
+					_: _,
+					query: function(params) {
+						return tripleStore.query(params)
+					}
 				},
 				wasm: false
 			})
-			vm.freeze(result, 'data') // adds immutable result dataspace to sandbox as data
 			try {
 				result = vm.run(query)
 				let used = Math.round(process.memoryUsage().heapUsed / 1024 / 1024);
@@ -258,26 +276,15 @@ async function main() {
 			data = JSONTag.clone(data)
 			Object.keys(data).forEach(key => {
 				if (Array.isArray(data[key])) {
-					data[key] = data[key].map((e,i) => {
-						if (e && typeof e === 'object') {
-							let id=JSONTag.getAttribute(e, 'id')
-							if (!id) {
-								id = baseURL+key+'/'+i+'/'
-							} else {
-								id = '#'+id
-							}
-							return new JSONTag.Link(id)
-						}
-						return e
-					})
+					data[key] = new JSONTag.Link(baseURL+key+'/')
 				} else if (typeof data[key] === 'object') {
-					let id=JSONTag.getAttribute(data[key], 'id')
-					if (!id) {
-						id = baseURL+key+'/'
-					} else {
-						id = '#'+id
+					if (JSONTag.getType(data[key])!=='link') {
+						let id=JSONTag.getAttribute(data[key], 'id')
+						if (!id) {
+							id = baseURL+key+'/'
+						}
+						data[key] = new JSONTag.Link(id)
 					}
-					data[key] = new JSONTag.Link(id)
 				}
 			})
 		}
