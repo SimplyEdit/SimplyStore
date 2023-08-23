8afa50bd61b8de8908c6ec16b0adf5874b379d16
Auke van Slooten <auke@muze.nl>
2023-08-23 16:39:07 +0200

    refactored code and added documentation

--- src/produce.mjs
+++ src/produce.mjs
@@ -1,50 +1,106 @@
 import {types} from 'node:util'
 import {clone} from '@muze-nl/jsontag/src/lib/functions.mjs'
 
+/**
+ * @TODO: when freezing changes, add them to the index
+ * @TODO: remove old entries (replaced) from the index, when creating a clone
+ * @TODO: don't add entries to the index that are already there
+ */
 
-export default function produce(original, updateFn) {
+/**
+ * Contains a list of references for each child or value object
+ * Usage:
+ *   const refs = references.get(valueObject)
+ * Returns an array of references in the form:
+ *   [
+ *   	{
+ *   		source: WeakRef,
+ *   		prop: String|Number
+ *   	}
+ *   ]
+ * You need to call source.deref() and then check that the result is not null
+ * @type {WeakMap}
+ */
+let references = new WeakMap()
+
+let seen       = new WeakMap()
+
+/**
+ * Adds a reference to the references index, which source object and property
+ * refer to which value object
+ * @param {object} source        The source or parent object
+ * @param {string|number} key    The property that refers the child or value object
+ * @param {object} value         The value or child object
+ */
+function addReference(source,key,value) {
+	if (value && typeof value === 'object' && Object.isFrozen(value)) {
+		if (!references.has(value)) {
+			references.set(value, [])
+		}
+		let list = references.get(value)
+		//@TODO: should filter out other references to source+key
+		list.push({
+			source: new WeakRef(source),
+			prop: key
+		})
+		if (!seen.has(value)) {
+			seen.set(value,true)
+			index(value)
+		}
+	}
+}
+
+/**
+ * Adds the given object and all its children to the references index
+ * The references index contains parent+property references that refer
+ * to each child object. References are added as WeakRef's, so need
+ * to be deref() to be used. Only objects are added to the reference,
+ * as literal values cannot be shared / referenced anyway.
+ * @param  {object} root The object to index
+ * @return {void}
+ */
+export function index(root) {
+	if (root && typeof root === 'object' && Object.isFrozen(root)) {
+		if (Array.isArray(root)) {
+			root.forEach((element, index) => {
+				addReference(root, index, element)
+			})
+		} else {
+			Object.entries(root).forEach(([prop, element]) => {
+				addReference(root, prop, element)
+			})
+		}
+	}
+}
+
+/**
+ * Returns an array with references for the child or value object
+ * If no references exist, it will return an empty array
+ * @param  {object} value The value or child object
+ * @return {array}        The list of references or an empty array
+ */
+export function findReferences(value) {
+	return references.get(value) || []
+}
+
+
+/**
+ * This function creates a new immutable datastructure from an existing one
+ * and an update function. The update function receives a single parameter, 
+ * which is a draft for the immutable datastructure. Each change set in the draft
+ * will appear in the new state
+ * This function is built to be compatible with [immer](https://immerjs.github.io/immer/)
+ * Except it will work on graphs, not just on trees. Also it uses @muze-nl/jsontag's clone
+ * function, so attributes and types will survive in the resulting dataset.
+ * 
+ * @param  {object}   baseState immutable datastructure to change
+ * @param  {function} updateFn  function that makes changes in the datastructure
+ * @return {object}             new immutable datastructure which incorporates the changes
+ */
+export default function produce(baseState, updateFn) {
 	let changes = []
-	let index   = new WeakMap()
-	let seen    = new WeakMap()
 	let values  = new Map()
 	let clones  = new Map()
-
-	function addReference(source,key,value) {
-		if (value && typeof value === 'object' && Object.isFrozen(value)) {
-			if (!index.has(value)) {
-				index.set(value, [])
-			}
-			let list = index.get(value)
-			//@TODO: should filter out other references to source+prop
-			list.push({
-				source: new WeakRef(source),
-				prop: key
-			})
-			console.log('added reference',source,key,value)
-			if (!seen.has(value)) {
-				seen.set(value,true)
-				createReferenceIndex(value)
-			}
-		}
-	}
-
-	function createReferenceIndex(root) {
-		if (root && typeof root === 'object' && Object.isFrozen(root)) {
-			if (Array.isArray(root)) {
-				root.forEach((element, index) => {
-					addReference(root, index, element)
-				})
-			} else {
-				Object.entries(root).forEach(([prop, element]) => {
-					addReference(root, prop, element)
-				})
-			}
-		}
-	}
-
-	function findReferences(target) {
-		return index.get(target) || []
-	}
 
 	function shallowClone(o) {
 		if (o instanceof Number) {
@@ -57,168 +113,166 @@ export default function produce(original, updateFn) {
 			return new String(o)
 		}
 		if (Array.isArray(o)) {
-			// @TODO: also copy ownProperties
 			return [ ...o ]
 		}
 		return { ...o }
 	}
 
-	function getClone(target) {
-		if (clones.has(target)) {
-			return clones.get(target)
+	/**
+	 * Returns the clone for this value object.
+	 * There is only ever one clone for each immutable baseState object
+	 * If the value object is immutable, it will create a clone or return the existing clone.
+	 * @param  {object} baseState The base value object
+	 * @return {object}           The clone for this value object
+	 */
+	function getClone(baseState) {
+		if (clones.has(baseState)) {
+			return clones.get(baseState)
 		}
-		if (Object.isFrozen(target)) {
-			console.log('cloning',target)
-			const c = clone(target)
+		if (Object.isFrozen(baseState)) {
+			const c = clone(baseState)
 			changes.push(c)
-			clones.set(target, c)
+			clones.set(baseState, c)
 			// add c to references index for any property value that is immutable
-			createReferenceIndex(c)
-			// find references to target
-			const references = findReferences(target)
-			console.log('references',references)
+			index(c)
+			// find references to baseState
+			const refs = findReferences(baseState)
 			// replace them with clone
-			references.forEach(r => {
+			refs.forEach(r => {
 				let source = r.source.deref()
 				if (!source) {
-					console.log('source freed',r)
 					return // continue
 				}
-				console.log('updating',source)
 				innerProduce(source, (draft) => {
-					let current = draft[r.prop]
-					if (types.isProxy(current)) {
-						console.log('deProxying ',r.prop,current)
-						current = values.get(current)
-						console.log('current is now', current)
-					} else {
-						console.log('no deProxying needed',r.prop,current)
-					}
-					if (current===target) { // reference hasn't been changed yet
+					let current = getRealValue(draft[r.prop])
+					if (current===getRealValue(baseState)) { // reference hasn't been changed yet
 						draft[r.prop] = c
-					} else {
-						console.log('source no longer points to target for prop', r.prop, source, target)
-						if (types.isProxy(target)) {
-							console.log('target is a proxy')
-						} else if (types.isProxy(source[r.prop])) {
-							console.log('source['+r.prop+'] is a proxy')
-						} else {
-							console.log('something else')
-						}
 					}
 				})
 			})
 			return c
 		}
-		return target // target is already a clone, since it is mutable
+		return baseState // baseState is already a clone, since it is mutable
 	}
 
-	function createProxy(original) 
+	function createProxy(baseState) 
 	{
-		const proxy = new Proxy({original}, updateHandler)
-		values.set(proxy, original)
+		const proxy = new Proxy({baseState}, updateHandler)
+		values.set(proxy, baseState)
 		return proxy
+	}
+
+	function getProxyValue(value) {
+		if (types.isProxy(value)) {
+			return value
+		}
+		if (value && typeof value === 'object') {
+			return createProxy(value)
+		}
+		return value
+	}
+
+	function getRealValue(value) {
+		if (types.isProxy(value)) {
+			value = values.get(value)
+		}
+		if (clones.has(value)) {
+			value = clones.get(value)
+		}
+		return value
+	}
+
+	function registerChange(clone, prop, value) {
+		if (value && typeof value === 'object') {
+			if (Object.isFrozen(value)) {
+				addReference(clone, prop, value)
+			} else {
+				changes.push(value) // makes sure it will get frozen
+			}
+		}
 	}
 
 	const updateHandler = {
 		get(target, prop, receiver) {
-//			console.log('get',prop)
-			if (Array.isArray(target.original) && target.original[prop] instanceof Function) {
-				switch(prop) {
-					case 'push': 
-//						console.log('array.push')
-						return (...args) => {
-//							console.log('array.push called',args)
-							let clone = getClone(target.original)
-							args.forEach(value => {
-								if (types.isProxy(value)) {
-									value = values.get(value)
-									if (clones.has(value)) {
-										value = clones.get(value)
-									}
-								}
-								clone.push(value)
-								if (value && typeof value === 'object') {
-									changes.push(value)
-									if (Object.isFrozen(value)) {
-										addReference(clone, prop, value)
-									}
-								}
-							})
-							return clone.length
+			if (Array.isArray(target.baseState) && target.baseState[prop] instanceof Function) {
+				return (...args) => {
+					let clone = getClone(target.baseState)
+					let before = shallowClone(clone)
+					let result = Array.prototype[prop].apply(clone, args)
+					// find differences
+					if (before.length>clone.length) {
+						for(let i=0,l=before.length-1;i++;i<=l) {
+							if (before[i]!==clone[i]) {
+								registerChange(clone, i, clone[i])
+							}
 						}
+					} else {
+						for(let i=0,l=clone.length;i++;i<=l) {
+							if (before[i]!==clone[i]) {
+								registerChange(clone, i, clone[i])
+							}
+						}
+					}
+					return result
+				}
+			} else if (Array.isArray(target.baseState)) {
+				switch(prop) {
+					case 'length':
+						return getRealValue(target.baseState).length
 					break
 				}
 			}
-			if (target.original[prop] && typeof target.original[prop] === 'object') {
-				return createProxy(target.original[prop])
-			}
-			return target.original[prop]
+			return getProxyValue(target.baseState[prop])
 		},
 		set(target, prop, value) {
-			let clone = getClone(target.original)
-
-			if (types.isProxy(value)) {
-				value = values.get(value)
-				if (clones.has(value)) {
-					value = clones.get(value)
-				}
-			}
-
+			let clone = getClone(target.baseState)
+			value = getRealValue(value)
 			clone[prop] = value
-
-			// if value is an object, make sure it gets frozen at the end as well
-			if (value && typeof value === 'object') {
-				changes.push(value) //@FIXME: don't do this if it already is a clone
-				if (Object.isFrozen(value)) {
-					addReference(clone, prop, value)
-				}
-			}
+			registerChange(clone, prop, value)
 			return true
 		},
 		deleteProperty(target, prop) {
-			let clone = getClone(target.original)
+			let clone = getClone(target.baseState)
 			delete clone[prop]
 		},
 		getOwnPropertyDescriptor(target, prop) {
+			baseState = target.baseState
+			if (clones.has(baseState)) {
+				baseState = clones.get(baseState)
+			}
 			// make sure properties are enumerable, configurable,
-			if (target.original.hasOwnProperty(prop)) {
+			if (baseState.hasOwnProperty(prop)) {
 				return {
 					configurable: true,
 					enumerable: true,
-					value: target.original[prop]
+					value: getProxyValue(baseState[prop])
 				}
 			}
-			//@FIXME: return what otherwise?
+			//return undefined
 		}
 	}
 
-	function innerProduce(original, updateFn) {
+	function innerProduce(baseState, updateFn) {
 		let proxy;
-		if (types.isProxy(original)) {
-			proxy = original
+		if (types.isProxy(baseState)) {
+			proxy = baseState
 		} else {
-			proxy = createProxy(original)
+			proxy = createProxy(baseState)
 		}
 		updateFn(proxy)
-		if (clones.has(original)) {
-			console.log('return new root')
-			return clones.get(original)
+		if (clones.has(baseState)) {
+			return clones.get(baseState)
 		}
-		console.log('no changes made')
-		return original // no changes were made
+		return baseState // no changes were made
 	}
 
-	console.log('creating index')
-	createReferenceIndex(original)
-	console.log('index done')
+	if (!references.size) {
+		index(baseState)
+	}
 
-	let newRoot = innerProduce(original, updateFn)
-
-	console.log('changes',changes)
+	let nextState = innerProduce(baseState, updateFn)
 	changes.forEach(entry => {
 		Object.freeze(entry)
 	})
-	console.log('dataspace', newRoot)
-	return newRoot
+	return nextState
 }
