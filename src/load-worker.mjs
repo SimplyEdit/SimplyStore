217f41c2ea414c32b29be7280f9bdea1fc22a4a2
Auke van Slooten <auke@muze.nl>
2024-01-24 13:45:49 +0100

    load worker now returns the id index (meta)

--- src/load-worker.mjs
+++ src/load-worker.mjs
@@ -4,16 +4,18 @@ import fs from 'fs'
 import stringify from './fastStringify.mjs'
 
 parentPort.on('message', datafile => {
-//	console.log('loading jsontag hier')
 	const jsontag = fs.readFileSync(datafile, 'utf-8')
 	let meta = {}
-	const data = JSONTag.parse(jsontag, meta)
+	const data = JSONTag.parse(jsontag)
 	const encoder = new TextEncoder()
-	let strData = stringify(data)
+	let strData = stringify(data, meta)
 	let buffer = encoder.encode(strData)
 
 	const sab = new SharedArrayBuffer(buffer.length)
 	let uint8sab = new Uint8Array(sab)
 	uint8sab.set(buffer,0)
-	parentPort.postMessage(uint8sab)
+	parentPort.postMessage({
+		data: uint8sab,
+		meta
+	})
 })
