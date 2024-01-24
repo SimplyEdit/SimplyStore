7ea243b43fcf0d5c17ff5ca941719a364527b9e8
Auke van Slooten <auke@muze.nl>
2024-01-24 13:47:35 +0100

    init now also gets the meta id index, which points to the index in the result set returned by parse so in the VM meta.index.id.get is a proxy for the real index, which dereferences that index and returns the correct object

--- src/query-worker-module.mjs
+++ src/query-worker-module.mjs
@@ -1,12 +1,29 @@
 import JSONTag from '@muze-nl/jsontag'
-import fastParse,{source} from './fastParse.mjs'
+import fastParse,{source, isProxy} from './fastParse.mjs'
 import {_,from,not,anyOf,allOf,asc,desc,sum,count,avg,max,min} from 'array-where-select'
 import pointer from 'json-pointer'
 import {VM} from 'vm2'
 import { memoryUsage } from 'node:process'
 
+let resultSet = []
 let dataspace
 let meta = {}
+let metaProxy = {
+    index: {
+    }
+}
+
+const metaIdProxy = {
+    get: (id) => {
+        let index = meta.index.id.get(id)
+        if (index) {
+            return resultSet[index]
+        }
+    },
+    has: (id) => {
+        return meta.index.id.has(id)
+    }
+}
 
 const FastJSONTag = {
     getType: (obj) => JSONTag.getType(obj[source]),
@@ -18,8 +35,11 @@ const FastJSONTag = {
 
 const tasks = {
 	init: async (task) => {
-		const jsontag = fastParse(task.req.body)
-		dataspace = jsontag[0]
+		resultSet = fastParse(task.req.body)
+		dataspace = resultSet[0]
+        meta = task.req.meta
+        metaProxy.index.id = metaIdProxy
+        //@TODO: add references and baseURL
 		return true
 	},
 	query: async (task) => {
@@ -33,6 +53,75 @@ const tasks = {
 }
 
 export default tasks
+
+export function runQuery(pointer, request, query) {
+    if (!pointer) { throw new Error('missing pointer parameter')}
+    if (!request) { throw new Error('missing request parameter')}
+	let response = {
+        jsontag: request.jsontag
+    }
+    let [result,path] = getDataSpace(pointer, dataspace)
+
+    if (query) {
+        // @todo add text search: https://github.com/nextapps-de/flexsearch
+        // @todo replace VM with V8 isolate
+        const vm = new VM({
+            timeout: 1000,
+            allowAsync: false,
+            sandbox: {
+                root: dataspace, //@TODO: if we don't pass the root, we can later shard
+                data: result,
+                meta: metaProxy,
+                _,
+                from,
+                not,
+                anyOf,
+                allOf,
+                asc,
+                desc,
+                sum,
+                count,
+                avg,
+                max,
+                min,    
+//                    console: connectConsole(res),
+                JSONTag: FastJSONTag,
+                request
+            },
+            wasm: false
+        })
+        try {
+            result = deProxy(vm.run(query))
+            let used = Math.round(process.memoryUsage().heapUsed / 1024 / 1024);
+            console.log(`(${used} MB)`);
+        } catch(err) {
+            console.log(err)
+            response.code = 422;
+            if (request.jsontag) {
+            	response.body = '<object class="Error">{"message":'+JSON.stringify(''+err)+',"code":422}'
+            } else {
+            	response.body = JSON.stringify({message:err, code: 422})
+            }
+        }
+    } else {
+        result = linkReplacer(result, path+'/')
+    }
+    if (!response.code) {
+        if (response.jsontag) {
+            try {
+            	response.body = JSONTag.stringify(result)
+            } catch(err) {
+                console.log(err)
+                response.code = 500
+                response.body = '<object class="Error">{"message":'+JSON.stringify(''+err)+',"code":500}'
+            }
+        } else {
+            //@FIXME: replace recursive links
+        	response.body = JSON.stringify(result)
+        }
+    }
+    return response
+}
 
 export function getDataSpace(path, dataspace) {
     if (path.substring(path.length-1)==='/') {
@@ -67,6 +156,9 @@ export function linkReplacer(data, baseURL) {
     } else if (type === 'link') {
         // do nothing
     } else if (data && typeof data === 'object') {
+        if (data[source]) {
+            data = data[source]
+        }
         data = JSONTag.clone(data)
         Object.keys(data).forEach(key => {
             if (Array.isArray(data[key])) {
@@ -85,71 +177,31 @@ export function linkReplacer(data, baseURL) {
     return data
 }
 
-export function runQuery(pointer, request, query) {
-    if (!pointer) { throw new Error('missing pointer parameter')}
-    if (!request) { throw new Error('missing request parameter')}
-	let response = {
-        jsontag: request.jsontag
+let seen = new WeakMap()
+function deProxy(o) {
+    if (!o) {
+        return o
     }
-    let [result,path] = getDataSpace(pointer, dataspace)
-
-    if (query) {
-        // @todo add text search: https://github.com/nextapps-de/flexsearch
-        // @todo replace VM with V8 isolate
-        const vm = new VM({
-            timeout: 1000,
-            allowAsync: false,
-            sandbox: {
-                root: dataspace, //@TODO: if we don't pass the root, we can later shard
-                data: result,
-                meta,
-                _,
-                from,
-                not,
-                anyOf,
-                allOf,
-                asc,
-                desc,
-                sum,
-                count,
-                avg,
-                max,
-                min,    
-//                    console: connectConsole(res),
-                JSONTag: FastJSONTag,
-                request
-            },
-            wasm: false
+    if (typeof o !== 'object') {
+        return o
+    }
+    if (seen.has(o)) {
+        return seen.get(o)
+    }
+    let result
+    if (Array.isArray(o)) {
+        result = o.map(deProxy)
+    } else if (JSONTag.isNull(o)) {
+        return o
+    } else if (JSONTag.getType(o)==='object' && o[source]) {
+        result = JSONTag.clone(o[source])
+        seen.set(o, result)
+        Object.entries(o[source]).forEach(([i,v]) => {
+            result[i] = deProxy(v)
         })
-        try {
-            result = vm.run(query)
-            let used = Math.round(process.memoryUsage().heapUsed / 1024 / 1024);
-            console.log(`(${used} MB)`);
-        } catch(err) {
-            console.log(err)
-            response.code = 422;
-            if (request.jsontag) {
-            	response.body = '<object class="Error">{"message":'+JSON.stringify(''+err)+',"code":422}'
-            } else {
-            	response.body = JSON.stringify({message:err, code: 422})
-            }
-        }
     } else {
-        result = linkReplacer(result, path+'/')
+        seen.set(o, o)
+        result = o
     }
-    if (!response.code) {
-        if (response.jsontag) {
-            try {
-            	response.body = JSONTag.stringify(result)
-            } catch(err) {
-                console.log(err)
-                response.code = 500
-                response.body = '<object class="Error">{"message":'+JSON.stringify(''+err)+',"code":500}'
-            }
-        } else {
-            //@FIXME: replace recursive links
-        	response.body = JSON.stringify(result)
-        }
-    }
-    return response
+    return result
 }
