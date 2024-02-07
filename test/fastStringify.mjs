6d6415dbc663741dab5919698b55d54e613e8c99
Auke van Slooten <auke@muze.nl>
2024-02-07 13:23:27 +0100

    added tests for parsing, immutability and changes

--- test/fastStringify.mjs
+++ test/fastStringify.mjs
@@ -1,5 +1,5 @@
 import JSONTag from '@muze-nl/jsontag'
-import stringify from '../src/fastStringify.mjs'
+import stringify, {resultSetStringify,stringToSAB} from '../src/fastStringify.mjs'
 import parse from '../src/fastParse.mjs'
 import tap from 'tap'
 
@@ -28,7 +28,7 @@ tap.test('Links', t => {
 		value: 'bar',
 		enumerable: false
 	})
-	let s = stringify(o,null,4)
+	let s = stringify(o)
 	t.equal(expect, s)
 	t.end()
 })
@@ -46,4 +46,48 @@ tap.test('Parse', t => {
 	t.equal(result[1].name, 'Foo')
 	t.equal(result[0].foo[0], result[1])
 	t.end()
+})
+
+tap.test('fastParse', t => {
+	let strData = `(23){"foo":[~1],"bar":[~2]}
+(64)<object class="foo" id="1">{"name":"Foo",#"nonEnumerable":"bar"}
+(57)<object class="bar" id="2">{"name":"Bar","children":[~1]}`
+	let sab = stringToSAB(strData)
+	let resultSet = parse(sab)
+	t.equal(resultSet[1].name, 'Foo')
+	t.equal(resultSet[2].name, 'Bar')
+	t.end()
+})
+
+tap.test('immutable', t => {
+	let strData = `(23){"foo":[~1],"bar":[~2]}
+(64)<object class="foo" id="1">{"name":"Foo",#"nonEnumerable":"bar"}
+(57)<object class="bar" id="2">{"name":"Bar","children":[~1]}`
+	let sab = stringToSAB(strData)
+	let resultSet = parse(sab, {}, true) // imutable
+	let root = resultSet[0]
+	root.foo[0].name='Baz'
+	t.equal(root.foo[0].name, 'Foo')
+	t.equal(resultSet[1].name, 'Foo')
+	t.end()
+})
+
+tap.test('update', t => {
+	let strData = `(23){"foo":[~1],"bar":[~2]}
+(64)<object class="foo" id="1">{"name":"Foo",#"nonEnumerable":"bar"}
+(57)<object class="bar" id="2">{"name":"Bar","children":[~1]}`
+	let expect = `(23){"foo":[~1],"bar":[~2]}
+(64)<object class="foo" id="1">{"name":"Baz",#"nonEnumerable":"bar"}
+(57)<object class="bar" id="2">{"name":"Bar","children":[~1]}`
+	
+	let sab = stringToSAB(strData)
+	let resultSet = parse(sab, {}, false)
+	let root = resultSet[0]
+	root.foo[0].name='Baz'
+	t.equal(root.foo[0].name, 'Baz')
+	t.equal(resultSet[1].name, 'Baz')
+	strData = resultSetStringify(resultSet)
+	t.equal(expect, strData)
+	t.end()
+
 })
