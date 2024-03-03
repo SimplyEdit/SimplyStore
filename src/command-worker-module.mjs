c81d5f2b39236c9625e99fcc10cf8f685bea751b
Auke van Slooten <auke@muze.nl>
2024-03-03 19:00:42 +0100

    fixed setting meta.index.id

--- src/command-worker-module.mjs
+++ src/command-worker-module.mjs
@@ -1,5 +1,5 @@
 import JSONTag from '@muze-nl/jsontag'
-import {source, isChanged} from './symbols.mjs'
+import {source, isChanged, getIndex} from './symbols.mjs'
 import fastParse from './fastParse.mjs'
 import {stringToSAB,resultSetStringify} from './fastStringify.mjs'
 import writeFileAtomic from 'write-file-atomic'
@@ -25,8 +25,16 @@ export const metaIdProxy = {
         })
     },
     set: (id,ref) => {
-        //FIXME: is this correct?
-        meta.index.id.set(id, resultSet.length-1)
+        if (!meta.index.id.has(id)) {
+            if (ref[getIndex]) {
+                meta.index.id.set(id, ref[getIndex])
+            } else {
+                throw new Error('cannot set index.id for non-proxy')
+            }
+        } else {
+            let line = meta.index.id.get(id)
+            resultSet[line] = ref
+        }
     },
     get: (id) => {
         let index = meta.index.id.get(id)
@@ -44,15 +52,16 @@ export const metaIdProxy = {
 }
 
 export const FastJSONTag = {
-    getType: (obj) => JSONTag.getType(obj[source]),
-    getAttribute: (obj, attr) => JSONTag.getAttribute(obj[source],attr),
+    getType: (obj) => JSONTag.getType(obj?.[source]),
+    getAttribute: (obj, attr) => JSONTag.getAttribute(obj?.[source],attr),
     setAttribute: (obj, attr, value) => {
+        if (!obj) return
         obj[isChanged] = true
         return JSONTag.setAttribute(obj[source], attr, value)
     },
-    getAttributes: (obj) => JSONTag.getAttributes(obj[source]),
-    getAttributeString: (obj) => JSONTag.getAttributesString(obj[source]),
-    getTypeString: (obj) => JSONTag.getTypeString(obj[source])
+    getAttributes: (obj) => JSONTag.getAttributes(obj?.[source]),
+    getAttributeString: (obj) => JSONTag.getAttributesString(obj?.[source]),
+    getTypeString: (obj) => JSONTag.getTypeString(obj?.[source])
 }
 
 export async function initialize(task) {
