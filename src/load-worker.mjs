5c422cc10a0304f4f48646696d80f19b76b92ed0
Auke van Slooten <auke@muze.nl>
2025-07-10 11:05:55 +0200

    load and save change files with their command id in the filename instead of a counter

--- src/load-worker.mjs
+++ src/load-worker.mjs
@@ -11,18 +11,24 @@ parentPort.on('message', (files) => {
 		}
 	}
 
+	const extension = files.dataFile.split('.').pop()
+	const basefile = files.dataFile.substring(0, files.dataFile.length - (extension.length + 1)) //+1 for . character
+
 	let count = 0
-	let basefile = files.dataFile
 	let data
 	let jsontag
 	let tempMeta = {}
 	let datafile = files.dataFile
+	let commands = files.commands
+	commands.push('done')
 	do {
-		jsontag = fs.readFileSync(datafile)
-		data = parse(jsontag, tempMeta) // tempMeta is needed to combine the resultArray, using meta conflicts with meta.index.id
-		count++
-		datafile = basefile + '.' + count
-	} while(fs.existsSync(datafile))
+		if (fs.existsSync(datafile)) {
+			jsontag = fs.readFileSync(datafile)
+			data = parse(jsontag, tempMeta) // tempMeta is needed to combine the resultArray, using meta conflicts with meta.index.id
+			count++
+		}
+		datafile = basefile + '.' + commands.shift() + '.' + extension
+	} while(commands.length)
 	meta.parts = count
 	if (files.schemaFile) {
 		jsontag = fs.readFileSync(files.schemaFile, 'utf-8')
