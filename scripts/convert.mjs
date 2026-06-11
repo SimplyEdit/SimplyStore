5ea94e0306f038fd7fab7e833439f050a214daf3
Auke van Slooten <auke@muze.nl>
2026-06-11 14:12:22 +0200

    allow overriding of indexFile and run index.create before write

--- scripts/convert.mjs
+++ scripts/convert.mjs
@@ -14,35 +14,52 @@ if (process.argv.length<=3) {
 // parse command line
 let inputFile = process.argv[2]
 let outputFile = process.argv[3]
-let indexFile = process.argv[4] || __dirname+'/../src/index.mjs'
+let indexFile = process.argv[4]
+if (indexFile && indexFile[0]!='/') {
+	indexFile = process.cwd()+'/'+indexFile
+} else if (!indexFile) {
+	indexFile = __dirname+'/../src/index.mjs'
+}
+let schemaFile = process.argv[5]
+if (schemaFile && schemaFile[0]!='/') {
+	schemaFile = process.cwd()+'/'+schemaFile
+}
 
 async function main() {
+	// now create indexes
+	console.log('Using index library:', indexFile)
+	
+	const index = await import(indexFile).then(mod => {
+	    return mod.default
+	})
+
+	let schema = {}
+	if (schemaFile) {
+		schema = JSONTag.parse(fs.readFileSync(schemaFile, 'utf-8'))
+	}
+
 	// load file
 	let input = fs.readFileSync(inputFile, 'utf-8')
 
 	// parse jsontag
 	let data = JSONTag.parse(input)
 
+	console.log('input data parsed')
 	// write resultset to output
 	let strData = stringify(serialize(data))
-	fs.writeFileSync(outputFile, strData)
-	console.log('Converted data written to ',outputFile)
 
-	// now create indexes
-	console.log('Using index library:', indexFile)
-	const index = await import(indexFile).then(mod => {
-	    return mod.default
-	})
+	console.log('od-jsontag created')
 
 	// indexes need the position data which is only available after
 	// parsing the od-jsontag data
-	const parser = new Parser()
+	const parser = new Parser('',false) // allow mutations
 	const odData = parser.parse(strData)
 
 	let meta = {
 		index: {
 			id: new Map()
 		},
+		schema,
 		resultArray: parser.meta.resultArray,
 		data: path.dirname(outputFile)
 	}
@@ -52,6 +69,11 @@ async function main() {
 
 	index.create(odData, meta)
 	console.log('Indexes created')
+
+	strData = stringify(serialize(odData))
+
+	fs.writeFileSync(outputFile, strData)
+	console.log('Converted data written to ',outputFile)
 }
 
 main()
